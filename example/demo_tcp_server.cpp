932550d0b1762e26d83b473d62fd837a981419a6
Sameh Kamal <sameh.kamaleldin@gmail.com>
2015-02-05 11:30:03 +0200

    Update tcp server example to accept cmd args

--- example/demo_tcp_server.cpp
+++ example/demo_tcp_server.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
-#include <tcp_server.hpp>
+#include <tcp/tcp_server.hpp>
+#include <sstream>
 
-int main()
+int main(int argc, char** argv)
 {
+  //initalize default port number and max connection cout
+  int port = 12001, max_connection_count= 1000;
 
-  TcpServer server(12001);
-  server.Listen(2000);
+  // check if there are any passed arguments
+  if(argc > 1)
+  {
+    // initialize string stream from argument
+    std::istringstream arg_stream(argv[1]);
+
+    // bind arguments stream to port int variable if valid
+    if ( !(arg_stream >> port) )
+      std::cerr << "Invalid number " << argv[1] << '\n';
+  }
+
+  // create server instance with specified port number
+  TcpServer server(port);
+
+  //start listening to connections
+  server.Listen(max_connection_count);
 
   return 0;
 }
