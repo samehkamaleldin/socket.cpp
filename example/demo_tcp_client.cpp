17f8d233e6838c3d84b8464c32367fae4ce46bf5
Sameh Kamal <sameh.kamaleldin@gmail.com>
2015-02-07 18:54:41 +0200

    Add tcp client class and demo example

---
+++ example/demo_tcp_client.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <tcp/tcp_client.hpp>
+#include <sstream>
+#include <string>
+
+using std::string;
+
+int main(int argc, char** argv)
+{
+
+  // initialize default port number and max connection cout
+  int     port    = 12001;
+  string  address = "127.0.0.1";
+
+  // check if there are any passed arguments
+  if(argc == 3)
+  {
+    // initialize string stream from argument
+    std::istringstream arg_stream(argv[1]);
+
+    // bind arguments stream to port int variable if valid
+    if ( !(arg_stream >> port) )
+      std::cerr << "Invalid number " << argv[1] << '\n';
+  }
+
+  // create server instance with specified port number
+  TcpClient client(address,port);
+
+  // connect to host node
+  client.Connect();
+
+  // send message to host
+  client.Send("Hi! - from client");
+
+  return 0;
+}
---
+++ src/tcp/tcp_client.hpp
@@ -0,0 +1,52 @@
+//=============================================================================================
+// CLASS       : TcpClient                                                                    =
+// Description : TCP Client module that creates and manages an instance of a tcp client       =
+//=============================================================================================
+
+#ifndef _TCP_CLIENT_HPP_
+#define _TCP_CLIENT_HPP_
+
+#include <sys/socket.h> // Needed for the socket functions
+#include <netdb.h>      // Needed for the socket functions
+#include <netinet/in.h> // Needed for internet addresses
+
+// library specific headers
+#include <net/node.hpp>
+#include <net/utils.hpp>
+
+using namespace std;
+
+class TcpClient : public Node
+{
+
+public:
+
+  // initialize a new tcp client instance
+  TcpClient(string, int);
+
+  // connect to host node
+  void Connect(void);
+
+  // send message to currently connected host node
+  void Send(string);
+
+  // send typed message to currently connected host node
+  void Send(string,string);
+
+  // send message to specific connected socket address
+  void Send(struct sockaddr*, string);
+
+  // send typed message to specific connected socket address
+  void Send(struct sockaddr*,string,string);
+
+  // Stop connection with host
+  void Stop();
+
+private:
+
+  // child proceess id
+  pid_t  _childpid;
+
+};
+
+#endif
